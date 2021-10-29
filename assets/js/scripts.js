ba593ef668d786daecbc2fd0539882ecc3647543
Olivertrain1221 <olivertrain1996@gmail.com>
2021-10-29 12:13:00 +0000

    Edit js to fix close modal bug

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,3 +25,37 @@ let playButton = document.getElementById("button-play");
 let trafficLight = document.getElementById("traffic-light");
 
 playButton.addEventListener("click", getUserName);
+
+
+function getUserName() {
+    let nameModal = document.getElementById("name-modal");
+    nameModal.style.display = "block";
+    let nameSubmitButton = document.getElementById("name-submit");
+    nameSubmitButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        let name = document.getElementById("name");
+        if (name.value) {
+            userName = name.value;
+            nameModal.style.display = "none";
+            console.log(userName);
+            mainContainer.style.display = "none";
+            gameStartup();
+        }
+
+
+
+    });
+};
+
+
+function closeModal() {
+    modalContainer.style.display = "none";
+    console.log("testing button")
+}
+// Closes the instructions modal
+function modalContainerClick(e) {
+    if (e.target == modalContainer) {
+        modalContainer.style.display = "none";
+        console.log("testing")
+    }
+}
