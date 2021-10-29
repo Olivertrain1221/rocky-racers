84a66c2241a58d9906c6379f6b6777873ab40f95
Olivertrain1221 <olivertrain1996@gmail.com>
2021-10-29 12:12:19 +0000

    Add js for my main container and instructions to open/close modal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,3 +9,19 @@ let closeInstructionButton = document.getElementById("close-button");
 // modalContainers event listeners to open/close
 buttonInstruction.addEventListener("click", openModal);
 closeInstructionButton.addEventListener("click", closeModal);
+
+
+// Event listener for whole window to run the modalContainerClick func.
+window.addEventListener("click", modalContainerClick);
+
+// Opens the instruction modal
+function openModal() {
+    modalContainer.style.display = "block";
+}
+
+// Getting main container and button elements to link with traffic lights.
+let mainContainer = document.getElementById("main-container");
+let playButton = document.getElementById("button-play");
+let trafficLight = document.getElementById("traffic-light");
+
+playButton.addEventListener("click", getUserName);
