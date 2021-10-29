c8f17250ab6cf53e8174a25366b8a1c409b7d254
Olivertrain1221 <olivertrain1996@gmail.com>
2021-10-29 12:11:40 +0000

    Add js variables

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+let userName = "";
+
+// modal container element assignement
+let modalContainer = document.getElementById("modal-container");
+let buttonInstruction = document.getElementById("button-instruction");
+let closeInstructionButton = document.getElementById("close-button");
+
+
+// modalContainers event listeners to open/close
+buttonInstruction.addEventListener("click", openModal);
+closeInstructionButton.addEventListener("click", closeModal);
